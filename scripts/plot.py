97daf41eebce821058a6e163dc1e91093d69fe28
AlexanderASidorov <alexander.a.sidorov@gmail.com>
2023-08-07 15:11:59 +0300

    Update plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -14,17 +14,22 @@ from alloy import Alloy
 from sigmoidal import Sigmoidal
 from factors import Factors
 import matplotlib.pyplot as plt
-from alloy import Composition
+from initial_data import Composition, t, T, delta_x, delta_T, delta_t
+from fraction import Fraction
+
 
 #Composition=Composition
 
-class Plot (Factors):
+class Plot (Fraction):
     
     def __init__(self):
         
-        self.Factors=Factors(Composition)
+        self.Factors=Factors()
+        self.Fraction=Fraction()
         
-        
+        self.T_int=self.Fraction.T_int
+        self.t_int=self.Fraction.t_int
+               
         #Temperature gaps
         self.temp_f=self.Factors.temp_f
         self.temp_p=self.Factors.temp_p
@@ -50,14 +55,17 @@ class Plot (Factors):
         fig01=plt.figure()
        
         # Ferrite curves
-        plt.plot(self.start_f, self.temp_f, '--', label="Start Ferrite")
+        plt.plot(self.start_f, self.temp_f,'--', label="Start Ferrite")
         plt.plot(self.end_f, self.temp_f, '--' ,label="End Ferrite")
         # Pearlite curves
         plt.plot(self.start_p, self.temp_p, label="Start Perlite")
         plt.plot(self.end_p, self.temp_p, label="End Perlite")
         # Bainite curves
-        plt.plot(self.start_b, self.temp_b, label="Start Bainite")
-        plt.plot(self.end_b, self.temp_b, label="End Bainite")
+        plt.plot(self.start_b, self.temp_b,'-.', label="Start Bainite")
+        plt.plot(self.end_b, self.temp_b,'-.', label="End Bainite")
+        
+        # Thermal cycle
+        plt.plot(self.t_int, self.T_int, label="Thermal curve", linewidth=3.0)
         
         
         # Plot settings
