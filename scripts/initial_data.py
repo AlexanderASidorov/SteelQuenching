12528076f55116f1b5b8a6e39e7f03cf16dcbf87
AlexanderASidorov <alexander.a.sidorov@gmail.com>
2023-08-11 23:37:43 +0300

    Update initial_data.py

--- scripts/initial_data.py
+++ scripts/initial_data.py
@@ -8,16 +8,16 @@ Created on Thu Aug  3 16:29:15 2023
 
 
 
-Composition={'GrainSize': 7.0, 'C': 0.6, 'Mn': 0.8, 
+Composition={'GrainSize': 5.0, 'C': 0.1, 'Mn': 0.4, 
        'Si': 0.15, "Ni": 0.04, 'Cr': 0.98, 'Mo': 0.21, 'W': 0.0, 'As':0.0, 'V': 0,
        'Cu': 0}
 
-t, T = [0, 1800], [800, 20]
+t, T = [0, 60], [800, 20]
 
 # fraction step (0.01 recomended)
 delta_x=0.05
-# time step (1 second reccomended)
-delta_t=0.5
+# time step (0.1 - 1)
+delta_t=0.1
 # Temperature step (1 Celcius recomended)
 delta_T=0.5
 
