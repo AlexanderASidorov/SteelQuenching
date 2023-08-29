b9dce44e6a543ab04c2293eacdb02d8e90002fed
AlexanderASidorov <alexander.a.sidorov@gmail.com>
2023-08-29 15:24:43 +0300

    Update initial_data.py

--- scripts/initial_data.py
+++ scripts/initial_data.py
@@ -8,16 +8,16 @@ Created on Thu Aug  3 16:29:15 2023
 
 
 # Steel 40H (see JMatPro demo)
-Composition={'GrainSize': 5.0, 'C': 0.7, 'Mn': 0.5, 
+Composition={'GrainSize': 5.0, 'C': 0.65, 'Mn': 0.5, 
        'Si': 1.45, "Ni": 0.2, 'Cr': 0.25, 'Mo': 0.0, 'W': 0.0, 'As':0.0, 'V': 0,
        'Cu': 0.03}
 
-t, T = [0, 60], [850, 60]
+t, T = [0, 1000], [850, 60]
 
 # fraction step (0.01 recomended)
-delta_x=0.25
+delta_x=0.025
 # time step (0.1 - 1)
-delta_t=0.25
+delta_t=0.1
 # Temperature step (1 Celcius recomended)
 delta_T=5
 # F
