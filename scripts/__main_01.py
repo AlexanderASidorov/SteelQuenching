5323af1ecea797f9285d44b40b187bc77c67ed07
AlexanderASidorov <alexander.a.sidorov@gmail.com>
2023-10-05 14:51:35 +0300

    Update __main_01.py

--- scripts/__main_01.py
+++ scripts/__main_01.py
@@ -18,11 +18,11 @@ from export import text, results2text, TTT2PandasDataframe, TTT2Excel
 Steel=Solve() # create an object of the Solve type class
 data=Steel.data # write main results into pandas dataframe
 TTT=Plot() # plot TTT diagramm
-text('data_max_grain_size_7.txt') # write data necessary for FlowVision simulation
-results = results2text(data, 'results_max_grain_size_7.txt') # write final phase composition
+text('data_max_plus_grain_size_7.txt') # write data necessary for FlowVision simulation
+results = results2text(data, 'results_max_plus_grain_size_7.txt') # write final phase composition
 Plot.plotting_phase_change(data) # plot phase change per time and Temperature
 TTT2Excel() # create Excel file with TTT data
-
+TTT_dataframe=TTT2PandasDataframe() # write TTT data into Pandas Dataframe
 
 # tau_f=Steel.Factors.tau_f
 # tau_p=Steel.Factors.tau_p
